fd4d6d16eca025128c2b5ba5b451900deff28e70
AbhiraajV <abhiraajverma@gmail.com>
2024-06-28 10:13:34 +0530

    route test for calc

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { StringCalculator } from "StringCalculator";
 
 const app = express();
-
+const calculator = new StringCalculator();
 app.get("/healthcheck", (req, res) => {
   res.send("Server is running");
 });
+
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
--- test/server/server.spec.ts
+++ test/server/server.spec.ts
@@ -9,3 +9,12 @@ describe("Testing Server and Routes", () => {
     expect(res.text).to.equal("Server is running");
   });
 });
+
+
+describe("GET /calc/add", () => {
+  it("should return 0 for an empty string", async () => {
+    const res = await request(app).get("/calc/add").query({ numbers: "" });
+    expect(res.status).to.equal(200);
+    expect(res.body.result).to.equal(0);
+  });
+});
